8df2abf124a65c444b5b7a4539b4bbdaef235600
Derrick Mei <derrick.mei.168@gmail.com>
2019-10-10 16:36:08 -0500

    extracted useEffect logic from index

---
+++ src/hooks/CustomUseStateEffectAxios.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+// don't freaking ask me why, but if you export Default,
+// you HAVE to name it with a capital letter as it's a "react component"
+export default function CustomUseStateEffectAxios() {
+    const [coinDataArray, setCoinDataArray] = useState([])
+
+    useEffect(() => {
+        axios
+          .get(
+            "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+          )
+          .then(res => setCoinDataArray(res.data))
+          .catch(err => console.log(err));
+      }, []);
+
+    return [coinDataArray, setCoinDataArray]
+}
